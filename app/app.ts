d4bb845c6a8f739d8bdc433521eea9e8b6a50104
Aleksey Glazkov <alexglazkov9703@gmail.com>
2020-06-23 18:21:53 -0400

    startup file changes

--- app/app.ts
+++ app/app.ts
@@ -10,13 +10,13 @@ let token: string = process.env.TOKEN || config.get("botTokenTest");
 let botOptions: TelegramBot.ConstructorOptions = {};
 
 if (process.env.NODE_ENV === 'production') {
-    logger.info('Production mode: Starting webhook');
-    let port: number = parseInt(process.env.PORT ?? '') || 3000;
+    let port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
     let host = '0.0.0.0';
     botOptions.webHook = {
         port,
         host
     };
+    logger.info(`Production mode: Starting on ${host}:${port}`);
 } else {
     logger.info('Dev mode: Starting polling');
     botOptions.polling = true;
@@ -24,7 +24,9 @@ if (process.env.NODE_ENV === 'production') {
 
 export const bot = new TelegramBot(token, botOptions);
 if (process.env.NODE_ENV === 'production') {
-    bot.setWebHook(process.env.HEROKU_URL + ':443/bot' + token);
+    let url = process.env.HEROKU_URL + ':443/bot' + token;
+    bot.setWebHook(url);
+    logger.info(`Webhook set to ${url}`);
 }
 
 export const db = connect();
