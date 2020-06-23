f61441e83952f7fe745da280151d33c0515bf6c9
Aleksey Glazkov <alexglazkov9703@gmail.com>
2020-06-22 22:55:52 -0400

    startup file changes

--- app/app.ts
+++ app/app.ts
@@ -5,7 +5,21 @@ import { connect } from "./database/database";
 import { startGame } from "./game/game";
 
 
-export const bot = new TelegramBot(config.get("botTokenTest"), { polling: true });
+//export const bot = new TelegramBot(config.get("botTokenTest"));
+let token: string = process.env.TOKEN || config.get("botTokenTest");
+let bot: TelegramBot;
+if (process.env.NODE_ENV === 'production') {
+    let host = process.env.HOST;
+    console.log(host);
+    bot = new TelegramBot(token);
+    if (process.env.HEROKU_URL) {
+        bot.setWebHook(process.env.HEROKU_URL + process.env.TOKEN);
+    }
+} else {
+    bot = new TelegramBot(token, { polling: true });
+}
 export const db = connect();
+
+export { bot };
 
 startGame();
