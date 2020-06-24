0b81981e4f12a14498f189fbff2df9eed4c0fdf4
Aleksey Glazkov <alexglazkov9703@gmail.com>
2020-06-23 23:10:26 -0400

    sorcery with startup files and configs

--- app/app.ts
+++ app/app.ts
@@ -9,27 +9,24 @@ import TelegramBot = require("node-telegram-bot-api");
 let token: string = process.env.TOKEN || config.get("botTokenTest");
 let botOptions: TelegramBot.ConstructorOptions = {};
 
-if (process.env.NODE_ENV === 'production') {
-    let port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-    let host = '0.0.0.0';
-    botOptions.webHook = {
+// if (process.env.NODE_ENV === 'production') {
+//     let port: number = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+//     botOptions.webHook = {
+//         port
+//     };
+//     logger.info(`Production mode: Starting on port ${port}`);
+// } else {
+//     logger.info('Dev mode: Starting polling');
+//     botOptions.polling = true;
+// }
 
-        port,
-        host
-    };
-    logger.info(`Production mode: Starting on ${host}:${port}`);
-} else {
-    logger.info('Dev mode: Starting polling');
-    botOptions.polling = true;
-}
-
+botOptions.polling = true;
 export const bot = new TelegramBot(token, botOptions);
-if (process.env.NODE_ENV === 'production') {
-    let url = process.env.HEROKU_URL + ':443/bot' + token;
-    bot.setWebHook(url);
-    bot.openWebHook();
-    logger.info(`Webhook set to ${url}`);
-}
+// if (process.env.NODE_ENV === 'production') {
+//     let url = process.env.HEROKU_URL + ':443/bot' + token;
+//     bot.setWebHook(url);
+//     logger.info(`Webhook set to ${url}`);
+// }
 
 export const db = connect();
 
